9522ad5ab1c63711b50c5b2c692a754e323da7f3
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-05-24 23:40:10 +0530

    Add fields to AdminDTO class for id, name, email, phone number, and active status

--- src/main/java/org/car/dto/AdminDTO.java
+++ src/main/java/org/car/dto/AdminDTO.java
@@ -1,5 +1,10 @@
 package org.car.dto;
 
 public class AdminDTO {
+    private String id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private boolean isActive;
 
 }
