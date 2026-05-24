dd78d31df2d2a57a1111af6e9e65debfbcfe5545
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-05-24 23:44:36 +0530

    Add fields to AdminEntity class for id, name, email, phone number, and active status

--- src/main/java/org/car/entity/AdminEntity.java
+++ src/main/java/org/car/entity/AdminEntity.java
@@ -1,4 +1,9 @@
 package org.car.entity;
 
 public class AdminEntity {
+    private String id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private boolean isActive;
 }
