99268a9108dbdf1c57b6110ac031147a0d66d9cd
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-13 09:59:22 +0530

    Add fields to RegisterDTO for user registration details

--- src/main/java/org/car/dto/Auth/RegisterDTO.java
+++ src/main/java/org/car/dto/Auth/RegisterDTO.java
@@ -1,4 +1,12 @@
 package org.car.dto.Auth;
 
 public class RegisterDTO {
+    private String Id;
+    private String username;
+    private String email;
+    private String password;
+    private String address;
+    private int phoneNumber;
+    private boolean isActive;
+
 }
