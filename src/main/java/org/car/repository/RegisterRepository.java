aee505c80f900f7933f366b3dc87f9d131c7bef3
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-13 11:24:08 +0530

    Change Id field type in RegisterRepository from String to Long

--- src/main/java/org/car/repository/RegisterRepository.java
+++ src/main/java/org/car/repository/RegisterRepository.java
@@ -3,5 +3,5 @@ package org.car.repository;
 import org.car.entity.Auth.RegisterEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RegisterRepository extends JpaRepository<RegisterEntity, String> {
+public interface RegisterRepository extends JpaRepository<RegisterEntity, Long> {
 }
