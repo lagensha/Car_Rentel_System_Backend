b188863e68bef2b8c20e26153805a1f2884acc63
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-13 10:09:05 +0530

    Add Lombok annotations to RegisterDTO for boilerplate reduction

--- src/main/java/org/car/dto/Auth/RegisterDTO.java
+++ src/main/java/org/car/dto/Auth/RegisterDTO.java
@@ -1,6 +1,12 @@
 package org.car.dto.Auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegisterDTO {
     private String Id;
     private String username;
