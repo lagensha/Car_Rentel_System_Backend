82f7b95cc5c7643320094355c61d5b3109d5d63f
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-13 11:12:22 +0530

    Add RegisterController with customer registration and management endpoints

--- src/main/java/org/car/controller/Auth/RegisterController.java
+++ src/main/java/org/car/controller/Auth/RegisterController.java
@@ -1,4 +1,36 @@
 package org.car.controller.Auth;
 
-public class RegisterController {
+import lombok.RequiredArgsConstructor;
+import org.car.dto.Auth.RegisterDTO;
+import org.car.service.impl.RegisterServiceImpl;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/auth")
+public class RegisterController{
+
+    final RegisterServiceImpl registerService;
+
+    @PostMapping("/register")
+    public void addCustomer(RegisterDTO registerDTO) {
+        registerService.addCustomer(registerDTO);
+    }
+
+
+    public void updateCustomer(RegisterDTO registerDTO) {
+
+    }
+
+
+    public void deleteCustomer(String id) {
+
+    }
+
+
+    public boolean isCustomerActive(String id) {
+        return false;
+    }
 }
