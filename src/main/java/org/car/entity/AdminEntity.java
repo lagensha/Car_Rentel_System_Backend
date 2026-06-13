8d7931023b773206c2e9d1ef552b4da9c925b295
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-13 10:10:02 +0530

    Add Lombok annotations to AdminEntity for boilerplate reduction

--- src/main/java/org/car/entity/AdminEntity.java
+++ src/main/java/org/car/entity/AdminEntity.java
@@ -1,5 +1,12 @@
 package org.car.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AdminEntity {
     private String id;
     private String name;
