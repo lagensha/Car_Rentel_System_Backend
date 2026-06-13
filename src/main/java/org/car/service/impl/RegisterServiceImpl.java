91d7eb90133a7c951a64984f22dae578b0d456bb
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-13 11:07:58 +0530

    Refactor RegisterServiceImpl to use ModelMapper for entity mapping

--- src/main/java/org/car/service/impl/RegisterServiceImpl.java
+++ src/main/java/org/car/service/impl/RegisterServiceImpl.java
@@ -2,20 +2,22 @@ package org.car.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.car.dto.Auth.RegisterDTO;
+import org.car.entity.Auth.RegisterEntity;
 import org.car.repository.RegisterRepository;
 import org.car.service.RegisterService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.ModelMap;
 
 @RequiredArgsConstructor
 @Service
 public class RegisterServiceImpl implements RegisterService {
     final RegisterRepository registerRepository;
-    final ModelMap modelMap;
+    final ModelMapper modelMapper;
 
     @Override
     public void addCustomer(RegisterDTO registerDTO) {
-
+        RegisterEntity registerEntity =modelMapper.map(registerDTO,RegisterEntity.class);
+        registerRepository.save(registerEntity);
     }
 
     @Override
