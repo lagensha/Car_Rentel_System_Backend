2222609efdf442a252b980ef95d71ce7c9d8fc70
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-13 09:59:53 +0530

    Add fields to RegisterEntity for user registration details

--- src/main/java/org/car/entity/Auth/RegisterEntity.java
+++ src/main/java/org/car/entity/Auth/RegisterEntity.java
@@ -1,4 +1,11 @@
 package org.car.entity.Auth;
 
 public class RegisterEntity {
+    private String Id;
+    private String username;
+    private String email;
+    private String password;
+    private String address;
+    private int phoneNumber;
+    private boolean isActive;
 }
