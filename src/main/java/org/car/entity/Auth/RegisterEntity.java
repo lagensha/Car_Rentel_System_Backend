ab33124c98f96847c204ef1bf96ae13dd3fd961d
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-13 10:25:09 +0530

    Add JPA annotations and Lombok support to RegisterEntity

--- src/main/java/org/car/entity/Auth/RegisterEntity.java
+++ src/main/java/org/car/entity/Auth/RegisterEntity.java
@@ -1,6 +1,20 @@
 package org.car.entity.Auth;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class RegisterEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String Id;
     private String username;
     private String email;
