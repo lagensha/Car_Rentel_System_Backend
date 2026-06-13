a234b9dc35600e05c4a76fc69795c9b1add94161
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-13 10:37:41 +0530

    Implement RegisterService interface in RegisterServiceImpl with customer management methods

--- src/main/java/org/car/service/impl/RegisterServiceImpl.java
+++ src/main/java/org/car/service/impl/RegisterServiceImpl.java
@@ -1,4 +1,26 @@
 package org.car.service.impl;
 
-public class RegisterServiceImpl {
+import org.car.dto.Auth.RegisterDTO;
+import org.car.service.RegisterService;
+
+public class RegisterServiceImpl implements RegisterService {
+    @Override
+    public void addCustomer(RegisterDTO registerDTO) {
+
+    }
+
+    @Override
+    public void updateCustomer(RegisterDTO registerDTO) {
+
+    }
+
+    @Override
+    public void deleteCustomer(String id) {
+
+    }
+
+    @Override
+    public boolean isCustomerActive(String id) {
+        return false;
+    }
 }
