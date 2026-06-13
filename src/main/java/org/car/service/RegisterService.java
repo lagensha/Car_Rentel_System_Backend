e4d7c3920ecf97a6bed3aa45f60d0f4c28e6f949
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-13 10:19:09 +0530

    Add methods for customer management in RegisterService interface

--- src/main/java/org/car/service/RegisterService.java
+++ src/main/java/org/car/service/RegisterService.java
@@ -1,4 +1,10 @@
 package org.car.service;
 
+import org.car.dto.Auth.RegisterDTO;
+
 public interface RegisterService {
+    void addCustomer(RegisterDTO registerDTO);
+    void updateCustomer(RegisterDTO registerDTO);
+    void deleteCustomer(String id);
+    boolean isCustomerActive(String id);
 }
