09af7afe6460465e320094a8339668ccb41c29c3
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-14 19:09:08 +0530

    Add searchByID and getAll methods to RegisterService interface

--- src/main/java/org/car/service/RegisterService.java
+++ src/main/java/org/car/service/RegisterService.java
@@ -2,9 +2,13 @@ package org.car.service;
 
 import org.car.dto.Auth.RegisterDTO;
 
+import java.util.List;
+
 public interface RegisterService {
     void addCustomer(RegisterDTO registerDTO);
     void updateCustomer(RegisterDTO registerDTO);
     void deleteCustomer(Long id);
     boolean isCustomerActive(Long id);
+    RegisterDTO searchByID(Long id);
+    List<RegisterDTO> getAll();
 }
