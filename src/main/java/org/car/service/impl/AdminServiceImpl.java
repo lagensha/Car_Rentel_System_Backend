4a2ff3a44afc564c5ab7b6908b798e719feab929
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-15 12:24:04 +0530

    Implement AdminService interface in AdminServiceImpl and add methods for car management and customer viewing

--- src/main/java/org/car/service/impl/AdminServiceImpl.java
+++ src/main/java/org/car/service/impl/AdminServiceImpl.java
@@ -1,4 +1,35 @@
 package org.car.service.impl;
 
-public class AdminServiceImpl {
+import org.car.service.AdminService;
+
+public class AdminServiceImpl implements AdminService {
+    @Override
+    public void addCar() {
+
+    }
+
+    @Override
+    public void updateCar() {
+
+    }
+
+    @Override
+    public void deleteCar() {
+
+    }
+
+    @Override
+    public void viewCar() {
+
+    }
+
+    @Override
+    public Boolean isCarAvailable() {
+        return null;
+    }
+
+    @Override
+    public void viewCustomer() {
+
+    }
 }
