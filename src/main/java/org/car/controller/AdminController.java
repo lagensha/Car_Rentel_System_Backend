0456ea413253402e90205dd727281accfc5923dc
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-15 13:51:30 +0530

    Refactor AdminController to use annotations for request mapping and improve method signatures

--- src/main/java/org/car/controller/AdminController.java
+++ src/main/java/org/car/controller/AdminController.java
@@ -4,10 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.car.dto.AdminDTO;
 import org.car.service.AdminService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RequiredArgsConstructor
@@ -17,16 +14,16 @@ public class AdminController {
 
     final AdminService adminService;
 @PostMapping("/login")
-    public void login(AdminDTO adminDTO) {
+    public void login(@RequestBody AdminDTO adminDTO) {
         adminService.login(adminDTO);
     }
 
 @DeleteMapping("/logout/{id}")
-    public void logout(Long id) {
+    public void logout(@PathVariable Long id) {
         adminService.logout(id);
     }
-
-    public ResponseEntity<String>toggleAdminStatus(Long id){
+@PutMapping("/toggleStatus/{id}")
+    public ResponseEntity<String>toggleAdminStatus(@PathVariable Long id){
             boolean active=adminService.isAdminActive(id);
             String status=active ? "Admin with ID "+id+" has been deactivated." : "Admin with ID "+id+" has been activated.";
             return ResponseEntity.ok(status);
