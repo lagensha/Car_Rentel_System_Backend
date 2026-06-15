776ec87e10c3980825d3f381f1d0193eb8bdae90
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-15 13:49:41 +0530

    Add toggleAdminStatus method to AdminController for activating/deactivating admins

--- src/main/java/org/car/controller/AdminController.java
+++ src/main/java/org/car/controller/AdminController.java
@@ -3,6 +3,7 @@ package org.car.controller;
 import lombok.RequiredArgsConstructor;
 import org.car.dto.AdminDTO;
 import org.car.service.AdminService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,7 +26,10 @@ public class AdminController {
         adminService.logout(id);
     }
 
-    public boolean isAdminActive(Long id) {
-        return false;
+    public ResponseEntity<String>toggleAdminStatus(Long id){
+            boolean active=adminService.isAdminActive(id);
+            String status=active ? "Admin with ID "+id+" has been deactivated." : "Admin with ID "+id+" has been activated.";
+            return ResponseEntity.ok(status);
     }
+
 }
