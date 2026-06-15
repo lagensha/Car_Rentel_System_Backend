98fac81ef0afd5d10554950e682ef8e8581a319b
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-15 13:17:43 +0530

    Fix AdminRepository to use AdminEntity for JPA repository mapping

--- src/main/java/org/car/repository/AdminRepository.java
+++ src/main/java/org/car/repository/AdminRepository.java
@@ -1,6 +1,7 @@
 package org.car.repository;
 
+import org.car.entity.AdminEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AdminRepository extends JpaRepository<AdminRepository, Long> {
+public interface AdminRepository extends JpaRepository<AdminEntity, Long> {
 }
