77037750ac4b7ef677317837b202208377ba6018
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-15 12:25:10 +0530

    Add AdminRepository and ModelMapper dependencies to AdminServiceImpl

--- src/main/java/org/car/service/impl/AdminServiceImpl.java
+++ src/main/java/org/car/service/impl/AdminServiceImpl.java
@@ -1,8 +1,16 @@
 package org.car.service.impl;
 
+import lombok.RequiredArgsConstructor;
+import org.car.repository.AdminRepository;
 import org.car.service.AdminService;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
+@Service
 public class AdminServiceImpl implements AdminService {
+    final AdminRepository adminRepository;
+    final ModelMapper modelMapper;
     @Override
     public void addCar() {
 
