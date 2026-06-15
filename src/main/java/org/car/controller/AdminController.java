9ca9d1dbd81c0e3dc9a2bb07b4a2942d05e73e7f
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-15 13:26:40 +0530

    Add RestController annotation and request mapping to AdminController

--- src/main/java/org/car/controller/AdminController.java
+++ src/main/java/org/car/controller/AdminController.java
@@ -2,9 +2,13 @@ package org.car.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.car.dto.AdminDTO;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
-
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/admin")
 public class AdminController  {
 
 
