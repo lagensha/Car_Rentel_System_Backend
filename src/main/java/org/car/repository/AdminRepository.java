8fae50db983cf960b613577d5fa33d5350713140
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-15 12:26:48 +0530

    Extend AdminRepository to inherit from JpaRepository for improved data access

--- src/main/java/org/car/repository/AdminRepository.java
+++ src/main/java/org/car/repository/AdminRepository.java
@@ -1,4 +1,6 @@
 package org.car.repository;
 
-public interface AdminRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepository extends JpaRepository<AdminRepository, Long> {
 }
