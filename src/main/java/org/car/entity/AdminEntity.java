138de742b56a6e8a181c6b1f484b91597059d6c9
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-15 12:27:24 +0530

    Add @Entity annotation to AdminEntity for JPA mapping

--- src/main/java/org/car/entity/AdminEntity.java
+++ src/main/java/org/car/entity/AdminEntity.java
@@ -1,5 +1,6 @@
 package org.car.entity;
 
+import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class AdminEntity {
     private Long id;
     private String name;
