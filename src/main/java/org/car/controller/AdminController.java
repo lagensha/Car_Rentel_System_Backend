153f3fa561d5ff84d42facce9b3eb6f3ddb70d6e
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-06-15 13:27:38 +0530

    Fix formatting in AdminController class

--- src/main/java/org/car/controller/AdminController.java
+++ src/main/java/org/car/controller/AdminController.java
@@ -10,9 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/admin")
-public class AdminController  {
+public class AdminController {
 
-        final AdminService adminService;
+    final AdminService adminService;
+
     public void login(AdminDTO adminDTO) {
 
     }
