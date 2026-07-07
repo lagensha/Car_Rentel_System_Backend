8b8f99b00b47319966276c6d56335087af13a458
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-07-07 11:31:28 +0530

    Add CarDTO class for transferring car data with relevant attributes

--- src/main/java/org/car/dto/CarDTO.java
+++ src/main/java/org/car/dto/CarDTO.java
@@ -1,4 +1,11 @@
 package org.car.dto;
 
 public class CarDTO {
+    private Long id;
+    private String model;
+    private  String brand;
+    private String fuelType;
+    private int seatingCapacity;
+    private double price;
+
 }
