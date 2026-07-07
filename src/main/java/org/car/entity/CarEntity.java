e1b0304fc3261582630d7a4445f672dbd510f158
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-07-07 11:33:04 +0530

    Add CarEntity class with attributes for car details

--- src/main/java/org/car/entity/CarEntity.java
+++ src/main/java/org/car/entity/CarEntity.java
@@ -1,4 +1,10 @@
 package org.car.entity;
 
 public class CarEntity {
+    private Long id;
+    private String model;
+    private  String brand;
+    private String fuelType;
+    private int seatingCapacity;
+    private double price;
 }
