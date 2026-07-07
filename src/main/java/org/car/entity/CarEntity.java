4aa8221eac8800323e2c5ede49624d613d5feef8
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-07-07 11:34:17 +0530

    Add annotations to CarEntity for JPA mapping and Lombok functionality

--- src/main/java/org/car/entity/CarEntity.java
+++ src/main/java/org/car/entity/CarEntity.java
@@ -1,6 +1,19 @@
 package org.car.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class CarEntity {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     private Long id;
     private String model;
     private  String brand;
