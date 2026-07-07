c1cf117388410c4fbf999acc106804494901b4ae
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-07-07 11:32:26 +0530

    Add Lombok annotations to CarDTO for boilerplate code reduction

--- src/main/java/org/car/dto/CarDTO.java
+++ src/main/java/org/car/dto/CarDTO.java
@@ -1,5 +1,12 @@
 package org.car.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CarDTO {
     private Long id;
     private String model;
