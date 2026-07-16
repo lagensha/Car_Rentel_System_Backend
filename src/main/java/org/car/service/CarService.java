aea1b194486f46a4992c9a2c0fd1660b5b00a059
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-07-16 09:26:10 +0530

    Add methods to CarService for car management operations

--- src/main/java/org/car/service/CarService.java
+++ src/main/java/org/car/service/CarService.java
@@ -1,4 +1,15 @@
 package org.car.service;
 
+import org.car.dto.Auth.RegisterDTO;
+
+import java.util.List;
+
 public interface CarService {
+    void addCar(RegisterDTO registerDTO);
+    void updateCar(RegisterDTO registerDTO);
+    void deleteCar(Long id);
+    boolean isCarAvailable(Long id);
+    RegisterDTO searchByID(Long id);
+    List<RegisterDTO> getAll();
+    boolean setStatus(Long id, boolean status);
 }
