48897dd3f807fbb3146a61780b4f4e636c158e73
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-07-16 09:24:27 +0530

    Extend CarRepository to inherit from JpaRepository for CRUD operations

--- src/main/java/org/car/repository/CarRepository.java
+++ src/main/java/org/car/repository/CarRepository.java
@@ -1,4 +1,7 @@
 package org.car.repository;
 
-public interface CarRepository {
+import org.car.entity.CarEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarRepository extends JpaRepository<CarEntity, Long> {
 }
