f60211fdb44e5fe25cf0c9a53d98459c18267698
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-07-16 09:27:14 +0530

    Implement CarService methods for car management operations

--- src/main/java/org/car/service/impl/CarServiceImpl.java
+++ src/main/java/org/car/service/impl/CarServiceImpl.java
@@ -1,4 +1,44 @@
 package org.car.service.impl;
 
-public class CarServiceImpl {
+import org.car.dto.Auth.RegisterDTO;
+import org.car.service.CarService;
+
+import java.util.List;
+
+public class CarServiceImpl implements CarService {
+
+    @Override
+    public void addCar(RegisterDTO registerDTO) {
+
+    }
+
+    @Override
+    public void updateCar(RegisterDTO registerDTO) {
+
+    }
+
+    @Override
+    public void deleteCar(Long id) {
+
+    }
+
+    @Override
+    public boolean isCarAvailable(Long id) {
+        return false;
+    }
+
+    @Override
+    public RegisterDTO searchByID(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<RegisterDTO> getAll() {
+        return List.of();
+    }
+
+    @Override
+    public boolean setStatus(Long id, boolean status) {
+        return false;
+    }
 }
